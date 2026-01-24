7d0cd3d5983f787b8903e5d528404632b25e0d42
Lttichai Yorach <04726ly@tse.com>
2026-01-24 12:05:47 +0700

    first

---
+++ IEM-Order/Form1.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace IEM_Order
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnRun;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(50, 50);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(190, 30);
+            this.btnRun.Text = "ISUZU";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 150);
+            this.Controls.Add(this.btnRun);
+            this.Text = "ISUZU";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ IEM-Order/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace IEM_Order
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "schtasks",
+                    Arguments = @"/run /s TSEDB /u tse\administrator /p scsadmin /tn ""IEM-Order.""",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process.Start(psi);
+
+                MessageBox.Show("สั่งรัน IEM-Order. สำเร็จ",
+                                "Success",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+    }
+}
